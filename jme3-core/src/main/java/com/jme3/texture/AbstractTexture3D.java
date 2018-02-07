e314503e9f891ea599fcede4975517ffa798133c
DESKTOP-M1KM0AN\Eelco <eelcobenard@gmail.com>
2018-02-07 16:27:39 +0100

    Commit na verwijderen duplicate code Texture3D / TextureCubeMap poging2

--- jme3-core/src/main/java/com/jme3/texture/AbstractTexture3D.java
+++ jme3-core/src/main/java/com/jme3/texture/AbstractTexture3D.java
@@ -1,90 +1,96 @@
 package com.jme3.texture;
 
+import java.io.IOException;
+
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
 import com.jme3.texture.Texture.WrapAxis;
 import com.jme3.texture.Texture.WrapMode;
 
 public abstract class AbstractTexture3D extends Texture {
 
-    private WrapMode wrapS = WrapMode.EdgeClamp;
-    private WrapMode wrapT = WrapMode.EdgeClamp;
-    private WrapMode wrapR = WrapMode.EdgeClamp;
-    @Override
-    public Texture createSimpleClone(Texture rVal) {
-        rVal.setWrap(WrapAxis.S, wrapS);
-        rVal.setWrap(WrapAxis.T, wrapT);
-        rVal.setWrap(WrapAxis.R, wrapR);
-        return super.createSimpleClone(rVal);
-    }
+	private WrapMode wrapS = WrapMode.EdgeClamp;
+	private WrapMode wrapT = WrapMode.EdgeClamp;
+	private WrapMode wrapR = WrapMode.EdgeClamp;
 
-    /**
-     * <code>setWrap</code> sets the wrap mode of this texture for a
-     * particular axis.
-     *
-     * @param axis
-     *            the texture axis to define a wrapmode on.
-     * @param mode
-     *            the wrap mode for the given axis of the texture.
-     * @throws IllegalArgumentException
-     *             if axis or mode are null
-     */
-    public void setWrap(WrapAxis axis, WrapMode mode) {
-        if (mode == null) {
-            throw new IllegalArgumentException("mode can not be null.");
-        } else if (axis == null) {
-            throw new IllegalArgumentException("axis can not be null.");
-        }
-        switch (axis) {
-            case S:
-                this.wrapS = mode;
-                break;
-            case T:
-                this.wrapT = mode;
-                break;
-            case R:
-                this.wrapR = mode;
-                break;
-        }
-    }
+	@Override
+	public Texture createSimpleClone(Texture rVal) {
+		rVal.setWrap(WrapAxis.S, wrapS);
+		rVal.setWrap(WrapAxis.T, wrapT);
+		rVal.setWrap(WrapAxis.R, wrapR);
+		return super.createSimpleClone(rVal);
+	}
 
-    /**
-     * <code>setWrap</code> sets the wrap mode of this texture for all axis.
-     *
-     * @param mode
-     *            the wrap mode for the given axis of the texture.
-     * @throws IllegalArgumentException
-     *             if mode is null
-     */
-    public void setWrap(WrapMode mode) {
-        if (mode == null) {
-            throw new IllegalArgumentException("mode can not be null.");
-        }
-        this.wrapS = mode;
-        this.wrapT = mode;
-        this.wrapR = mode;
-    }
+	/**
+	 * <code>setWrap</code> sets the wrap mode of this texture for a particular
+	 * axis.
+	 *
+	 * @param axis
+	 *            the texture axis to define a wrapmode on.
+	 * @param mode
+	 *            the wrap mode for the given axis of the texture.
+	 * @throws IllegalArgumentException
+	 *             if axis or mode are null
+	 */
+	public void setWrap(WrapAxis axis, WrapMode mode) {
+		if (mode == null) {
+			throw new IllegalArgumentException("mode can not be null.");
+		} else if (axis == null) {
+			throw new IllegalArgumentException("axis can not be null.");
+		}
+		switch (axis) {
+		case S:
+			this.wrapS = mode;
+			break;
+		case T:
+			this.wrapT = mode;
+			break;
+		case R:
+			this.wrapR = mode;
+			break;
+		}
+	}
 
-    /**
-     * <code>getWrap</code> returns the wrap mode for a given coordinate axis
-     * on this texture.
-     *
-     * @param axis
-     *            the axis to return for
-     * @return the wrap mode of the texture.
-     * @throws IllegalArgumentException
-     *             if axis is null
-     */
-    public WrapMode getWrap(WrapAxis axis) {
-        switch (axis) {
-            case S:
-                return wrapS;
-            case T:
-                return wrapT;
-            case R:
-                return wrapR;
-        }
-        throw new IllegalArgumentException("invalid WrapAxis: " + axis);
-    }
-	
+	/**
+	 * <code>setWrap</code> sets the wrap mode of this texture for all axis.
+	 *
+	 * @param mode
+	 *            the wrap mode for the given axis of the texture.
+	 * @throws IllegalArgumentException
+	 *             if mode is null
+	 */
+	public void setWrap(WrapMode mode) {
+		if (mode == null) {
+			throw new IllegalArgumentException("mode can not be null.");
+		}
+		this.wrapS = mode;
+		this.wrapT = mode;
+		this.wrapR = mode;
+	}
+
+	/**
+	 * <code>getWrap</code> returns the wrap mode for a given coordinate axis on
+	 * this texture.
+	 *
+	 * @param axis
+	 *            the axis to return for
+	 * @return the wrap mode of the texture.
+	 * @throws IllegalArgumentException
+	 *             if axis is null
+	 */
+	public WrapMode getWrap(WrapAxis axis) {
+		switch (axis) {
+		case S:
+			return wrapS;
+		case T:
+			return wrapT;
+		case R:
+			return wrapR;
+		}
+		throw new IllegalArgumentException("invalid WrapAxis: " + axis);
+	}
 
 	public WrapMode getWrapS() {
 		return wrapS;
@@ -108,6 +114,24 @@ public abstract class AbstractTexture3D extends Texture {
 
 	public void setWrapR(WrapMode wrapR) {
 		this.wrapR = wrapR;
+	}
+
+	@Override
+	public void write(JmeExporter e) throws IOException {
+		super.write(e);
+		OutputCapsule capsule = e.getCapsule(this);
+		capsule.write(this.wrapS, "wrapS", WrapMode.EdgeClamp);
+		capsule.write(this.wrapT, "wrapT", WrapMode.EdgeClamp);
+		capsule.write(this.wrapR, "wrapR", WrapMode.EdgeClamp);
+	}
+
+	@Override
+	public void read(JmeImporter e) throws IOException {
+		super.read(e);
+		InputCapsule capsule = e.getCapsule(this);
+		wrapS = capsule.readEnum("wrapS", WrapMode.class, WrapMode.EdgeClamp);
+		wrapT = capsule.readEnum("wrapT", WrapMode.class, WrapMode.EdgeClamp);
+		wrapR = capsule.readEnum("wrapR", WrapMode.class, WrapMode.EdgeClamp);
 	}
 
 	@Override
